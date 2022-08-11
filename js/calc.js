1c97491cac11ee941ef695693a554571fb8bc298
FerLagHer <flagunahermida@gmail.com>
2022-08-11 19:38:59 -0300

    Accurate multiplication and division

--- js/calc.js
+++ js/calc.js
@@ -1,23 +1,37 @@
 //Input
-const numpad = document.querySelector('.calc__numpad').children;
-const operators = document.querySelector('.calc__operators').children;
-const clear = document.querySelector('.calc__clear').children;
-const memory = document.querySelector('.calc__memory').children;
-const percent = document.querySelector('.calc__percent').children;
-const sign = document.querySelector('.calc__sign').children;
-const tax = document.querySelector('.calc__tax').children;
-const margin = document.querySelector('.calc__margin').children;
-const items = document.querySelector('.calc__items').children;
+const input = {
+    numpad: document.querySelector('.calc__numpad').children,
+    operators: document.querySelector('.calc__operators').children,
+    clear: document.querySelector('.calc__clear').children,
+    memory: document.querySelector('.calc__memory').children,
+    percent: document.querySelector('.calc__percent').children,
+    sign: document.querySelector('.calc__sign').children,
+    tax: document.querySelector('.calc__tax').children,
+    margin: document.querySelector('.calc__margin').children,
+    items: document.querySelector('.calc__items').children,
+}
 
 //Output
-const display = document.querySelector('.calc__digit');
-const tape = document.querySelector('.tape__list');
+const output = {
+    display: document.querySelector('.calc__digit'),
+    tape: document.querySelector('.tape__list'),
+    updateDisplay: function (data) {this.display.innerHTML = data},
+    print: function (int, op) {
+        const li = document.createElement('li');
+        if (op === '-' || int < 0) {
+            li.classList.add("tape__minus");
+        };
+        li.innerHTML = toDecimal(int).toFixed(2) + ' ' + op;
+        output.tape.appendChild(li);
+    }
+}
 
 //Memory
 let currentDigits = '0';
 let currentSum = [];
 let total = 0;
 let grandTotal = 0;
+let factor = 1;
 let isMultiplicand = false;
 let isDividend = false;
 
@@ -25,57 +39,44 @@ let isDividend = false;
 const toInteger = float => Math.round(parseFloat(float) * 100);
 const toDecimal = integer => integer / 100;
 
-const print = (int, op) => {
-    const li = document.createElement('li');
-    if (op === '-' || int < 0) {
-        li.classList.add("tape__minus");
-    };
-    li.innerHTML = toDecimal(int).toFixed(2) + ' ' + op;
-    tape.appendChild(li);
-}
-
 //Record number keys
-for (let i = 0; i < numpad.length; i++) {
-    numpad[i].addEventListener('click', e => {
-        let id = numpad[i].id;
+for (let i = 0; i < input.numpad.length; i++) {
+    input.numpad[i].addEventListener('click', e => {
+        let id = input.numpad[i].id;
         if ((id.includes('0') && currentDigits === '0') || (id === '.' && currentDigits.includes('.'))) {
-            display.innerHTML = currentDigits;
+            output.updateDisplay(currentDigits);
         }
         else if (id === '.' && currentDigits === '0') {
             currentDigits = '0.'
-            display.innerHTML = currentDigits;
+            output.updateDisplay(currentDigits);
         }
         else if (currentDigits === '0') {
             currentDigits = id;
-            display.innerHTML = currentDigits;
+            output.updateDisplay(currentDigits);
         }
         else {
             currentDigits += id;
             if (currentDigits >= 999999999999) {
                 currentDigits = 999999999999
             }
-            display.innerHTML = currentDigits;
+            output.updateDisplay(currentDigits);
         }
     })
 };
 
 //Arithmetic
-for (let i = 0; i < operators.length; i++) {
-    operators[i].addEventListener('click', e => {
-        let currentOperator = operators[i].id;
+for (let i = 0; i < input.operators.length; i++) {
+    input.operators[i].addEventListener('click', e => {
+        let currentOperator = input.operators[i].id;
+        let integerDigits = toInteger(currentDigits)
 
         const addAndPrint = op => {
             if (currentDigits === '0') {
-                if (isMultiplicand || isDividend) {
-                    print(total, op);
-                }
-                else {
-                    //Do nothing
-                }
+                //Do nothing
             }
             else {
-                const roundDigits = toInteger(currentDigits);
-                print(roundDigits, op);
+                const roundDigits = integerDigits;
+                output.print(roundDigits, op);
                 currentDigits = '0';
                 if (op === '+' || op === '-'){
                     total += parseInt(op+roundDigits);
@@ -83,84 +84,64 @@ for (let i = 0; i < operators.length; i++) {
                 else {
                     total += parseInt(roundDigits);
                 }
-                display.innerHTML = toDecimal(total);
+                output.updateDisplay(toDecimal(total));
             }
         };
 
-        const multAndDiv = op => {
-            print (toInteger(currentDigits), op);
-            display.innerHTML = toDecimal(total);
+        const printProduct = op => {
+            if (op === '='){
+                output.print (integerDigits, op);
+                output.updateDisplay(toDecimal(total));
+            }
+            else {
+                output.print (factor, op);
+            }
             currentDigits = '0';
-            isMultiplicand = false;
-            isDividend = false;
-        };
+            isMultiplicand = op === '×';
+            isDividend = op === '÷';
+        }
 
-        if (currentOperator === '+') {
-            if (isMultiplicand) {
-                total = (total * toInteger(currentDigits)) / 100;
-                multAndDiv('=');
-                print(total, '+');
+        if (currentOperator === '+' || currentOperator === '-') {
+            if (isMultiplicand && currentOperator === '+') {
+                total += (factor * integerDigits) / 100;
+                printProduct('=');
+                output.print(total, currentOperator);
             }
-            else if (isDividend) {
-                total = (total / toInteger(currentDigits)) * 100;
-                multAndDiv('=');
-                print(total, '+');
+            else if (isDividend && currentOperator === '+') {
+                total += (factor / integerDigits) * 100;
+                printProduct('=');
+                output.print(total, currentOperator);
             }
             else {
                 addAndPrint(currentOperator)
             }
         }
 
-        else if (currentOperator === '-') {
-            addAndPrint(currentOperator)
-        }
-
-        else if (currentOperator === '×') {
+        else if (currentOperator === '×' || currentOperator === '÷') {
+            factor = integerDigits;
             if (total === 0 && currentDigits === '0') {
                 //Do nothing
             }
-            else {
-                if (isMultiplicand) {
-                    total = (total * toInteger(currentDigits)) / 100;
-                    multAndDiv(currentOperator)
-                    isMultiplicand = true;
-                }
-                else if (isDividend) {
-                    total = (total / toInteger(currentDigits)) * 100;
-                    multAndDiv(currentOperator);
-                    isMultiplicand = true;
-                }
-                else {
-                    isMultiplicand = true;
-                    addAndPrint(currentOperator);
-                }
+            else if (isMultiplicand) {
+                total = (factor * integerDigits) / 100;
+                printProduct(currentOperator)
             }
-        }
-        
-        else if (currentOperator === '÷') {
-            if (total === 0 && currentDigits === '0') {
-                //Do nothing
+            else if (isDividend) {
+                total = (factor / integerDigits) * 100;
+                printProduct(currentOperator);
             }
             else {
-                if (isDividend) {
-                    total = (total / toInteger(currentDigits)) * 100;
-                    multAndDiv(currentOperator);
-                    isDividend = true;
-                }
-                else if (isMultiplicand) {
-                    total = (total * toInteger(currentDigits)) / 100;
-                    multAndDiv(currentOperator);
-                    isDividend = true;
+                isMultiplicand = currentOperator === '×';
+                isDividend = currentOperator === '÷';
+                if (currentDigits === '0') {
+                    factor = total;
+                    printProduct(currentOperator)
+                    total = 0;
                 }
                 else {
-                    isDividend = true;
-                    addAndPrint(currentOperator);
+                    printProduct(currentOperator)
                 }
             }
         }
     })
 };
-
-//Things to fix:
-//1+1*2=4
-//Sum and rest zero, yes or no?
